558338f44acf7aa16cd44eacd09085e11e985502
Bosnyák Imre Dávid <suti211@gmail.com>
2017-02-28 13:48:36 +0100

    Implemented DiseaseCard concrete class

---
+++ game/DiseaseCard.java
@@ -0,0 +1,42 @@
+package game;
+
+import model.Card;
+
+public class DiseaseCard extends Card {
+	
+	private int lethality;
+	private double victims;
+	private short incubationTime;
+	
+	public DiseaseCard(String name, int lethality, double victims, short incubationTime) {
+		this.name = name;
+		this.lethality = lethality;
+		this.victims = victims;
+		this.incubationTime = incubationTime;
+	}
+	
+	public int getLethality() {
+		return lethality;
+	}
+	
+	public double getVictims() {
+		return victims;
+	}
+	
+	public short getIncubationTime() {
+		return incubationTime;
+	}
+
+	@Override
+	public String toString() {
+		return "DiseaseCard [lethality=" + lethality + ", victims=" + victims + ", incubationTime=" + incubationTime
+				+ ", name=" + name + "]";
+	}
+	
+	
+
+	
+	
+	
+
+}
