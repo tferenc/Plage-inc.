c8255312230b0a670776e7ffd6efecc4e40b4d21
Bosnyák Imre Dávid <suti211@gmail.com>
2017-02-28 15:20:33 +0100

    Implemented Hand.java class

--- player/Hand.java
+++ player/Hand.java
@@ -1,29 +1,29 @@
 package player;
+
 import game.DiseaseCard;
 import model.Deck;
 
 public class Hand {
 	private Deck hand;
 	private boolean isEmpty;
-	
-	public boolean isEmpty()
-	{
-		
-		return false;
+
+	public boolean isEmpty() {
+		if (hand.getCards().size() > 0) {
+			return false;
+		} else {
+			return true;
+		}
 	}
-	
-	public void addCard(DiseaseCard card)
-	{
-		
+
+	public void addCard(DiseaseCard card) {
+		hand.getCards().add(card);
 	}
-	
-	public Deck getDeck()
-	{
-		
+
+	public Deck getHand() {
+		return hand;
 	}
-	
-	public DiseaseCard getTopCard()
-	{
-		
+
+	public DiseaseCard getTopCard() {
+		return hand.getCards().get(0);
 	}
 }
