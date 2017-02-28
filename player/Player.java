01f466a4b9f6aa63ed0a53b46f6b7ab685ca73e8
redgyuf <faramirex070@gmail.com>
2017-02-28 14:08:24 +0100

    Player skeleton made

--- player/Player.java
+++ player/Player.java
@@ -1,5 +1,30 @@
 package player;
 
-public class Player {
+import java.util.Scanner;
 
+public class Player {
+	
+	private Hand hand;
+	private String name;
+	
+	public Player(Hand hand, String name) {
+		this.hand = hand;
+		this.name = name;
+	}
+
+	public Hand getHand(){
+		return hand;
+	}
+	
+	public void giveCard(DiseaseCard card, Player target){
+		
+	}
+	
+	public String chooseStat(){
+		Scanner sc = new Scanner(System.in);
+	}
+	
+	public String toString(){
+		return null;
+	}
 }
