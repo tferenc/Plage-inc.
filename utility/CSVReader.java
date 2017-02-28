db8dadb3c4814a981cab7f2726cfed16127044b1
Bosnyák Imre Dávid <suti211@gmail.com>
2017-02-28 15:00:12 +0100

    Implemented card instantiation in CSVReader, added constructor getter

--- utility/CSVReader.java
+++ utility/CSVReader.java
@@ -5,25 +5,45 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Vector;
+
+import game.DiseaseCard;
 
 public class CSVReader {
 
-    private String filepath = "Plage-inc./diseases.csv";
+	private static final int NAME = 0;
+	private static final int LETHALITY = 1;
+	private static final int VICTIMS = 2;
+	private static final int INCUBATION = 3;
+	private String filepath = "Plage-inc./diseases.csv";
+    private Vector<DiseaseCard> deck = new Vector<DiseaseCard>();
+    
+    public CSVReader(){
+    	readInCards();
+    }
+    
+    public Vector<DiseaseCard> getDeck(){
+    	return this.deck;
+    }
 
-    public DiseaseCard[] readlnCards(filepath) {
+    public void readInCards() {
         BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
 
         try {
             br = new BufferedReader(new FileReader(filepath));
+            
             while ((line = br.readLine()) != null) {
 
                 String[] virus = line.split(cvsSplitBy);
 
-                System.out.println("Vírus neve : " + virus[0] + "," + " halálozási ráta: " + virus[1] + "," + " Halálozás/100 000 fő "
-                        + virus[2] + "," + " Lappangási idő (nap): " + virus[3]);
-
+                deck.add(new DiseaseCard(
+                		virus[NAME],
+						Integer.parseInt(virus[LETHALITY]), 
+						Double.parseDouble(virus[VICTIMS]), 
+						Short.parseShort(virus[INCUBATION])));
+                
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
