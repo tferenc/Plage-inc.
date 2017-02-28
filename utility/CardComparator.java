70e3723e09ee8e298154fb9a15061d8811b225d6
redgyuf <faramirex070@gmail.com>
2017-02-28 14:08:12 +0100

    CardComparator skeleton made

--- utility/CardComparator.java
+++ utility/CardComparator.java
@@ -1,5 +1,14 @@
 package utility;
 
-public class CardComparator {
+import player.Player;
+import model.Table;
 
+public class CardComparator {
+	public Player compare(Player player1, Player player2){
+		Player winner;
+		
+		
+		//return player2.getHand().getTopCard();
+		return null;
+	}
 }
