03220bd4a8aa263a78323fdfb122867407e15063
Bosnyák Imre Dávid <suti211@gmail.com>
2017-03-01 13:41:22 +0100

    Started implementing main class

--- Main.java
+++ Main.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
+import game.DiseaseCardGame;
+import player.Player;
+import utility.Logger;
 
 public class Main {
 
+	
+
 	public static void main(String[] args) {
+		Logger logger = new Logger();
 		DiseaseCardGame game = new DiseaseCardGame();
-		game.start();
+		
+		game.initializePlayers(logger);
+		setUpplayerNames(game, logger);
+		game.setStartingPlayer(logger);
+		game.printDealer(logger);
+		
+		logger.print(game.getTable().getPlayers()[0].getHand().getTopCard().toString());
+	}
+
+	public static void setUpplayerNames(DiseaseCardGame game, Logger logger){
+		Player[] players = game.getTable().getPlayers();
+		Scanner sc = new Scanner(System.in);
+		
+		for (int i = 0; i < players.length; i++) {
+			game.setPlayerName(players[i], i, logger, sc);
+		}
 	}
 
 }
