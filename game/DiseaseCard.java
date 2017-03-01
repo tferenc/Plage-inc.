9eaeb9f361ced3638977b830dd6d7f32733630d2
redgyuf <faramirex070@gmail.com>
2017-03-01 16:01:57 +0100

    getter for CardName

--- game/DiseaseCard.java
+++ game/DiseaseCard.java
@@ -19,6 +19,10 @@ public class DiseaseCard extends Card {
 		this.incubationTime = incubationTime;
 	}
 	
+	public String getName(){
+		return name;
+	}
+	
 	public int getLethality() {
 		return lethality;
 	}
